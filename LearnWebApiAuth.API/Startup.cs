2fa6ed84429de7c3f473975a1baa45b87e6ac186
RamiV <rami.ramilu@gmail.com>
2016-03-14 16:26:42 +0530

    1. Removed Global.asax 2. Added OWIN Startup.cs class 3. changed JsonMediaTypeFormatter to use CamelCase property names.

---
+++ LearnWebApiAuth.API/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(LearnWebApiAuth.API.Startup))]
+
+namespace LearnWebApiAuth.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
