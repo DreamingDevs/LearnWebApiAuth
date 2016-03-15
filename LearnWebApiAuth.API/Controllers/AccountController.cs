3238378630c81e0f0f2a6a8bf219f068ca091f5d
RamiV <rami.ramilu@gmail.com>
2016-03-15 23:18:11 +0530

    Added - 1. Account Controller 2. CORS Support 3. OAuth Server PRovider

--- LearnWebApiAuth.API/Controllers/AccountController.cs
+++ LearnWebApiAuth.API/Controllers/AccountController.cs
@@ -14,30 +14,23 @@ namespace LearnWebApiAuth.API.Controllers
     public class AccountController : ApiController
     {
         private AuthRepository _repo = null;
-
         public AccountController()
         {
             _repo = new AuthRepository();
         }
-
-        // POST api/Account/Register
+        
         [AllowAnonymous]
         [Route("Register")]
         public async Task<IHttpActionResult> Register(UserModel userModel)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
 
             IdentityResult result = await _repo.RegisterUser(userModel);
-
             IHttpActionResult errorResult = GetErrorResult(result);
 
             if (errorResult != null)
-            {
                 return errorResult;
-            }
 
             return Ok();
         }
@@ -45,9 +38,7 @@ namespace LearnWebApiAuth.API.Controllers
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
                 _repo.Dispose();
-            }
 
             base.Dispose(disposing);
         }
@@ -55,9 +46,7 @@ namespace LearnWebApiAuth.API.Controllers
         private IHttpActionResult GetErrorResult(IdentityResult result)
         {
             if (result == null)
-            {
                 return InternalServerError();
-            }
 
             if (!result.Succeeded)
             {
@@ -70,10 +59,7 @@ namespace LearnWebApiAuth.API.Controllers
                 }
 
                 if (ModelState.IsValid)
-                {
-                    // No ModelState errors are available to send, so just return an empty BadRequest.
                     return BadRequest();
-                }
 
                 return BadRequest(ModelState);
             }
--- LearnWebApiAuth.API/Startup.cs
+++ LearnWebApiAuth.API/Startup.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using Microsoft.Owin.Cors;
 
 [assembly: OwinStartup(typeof(LearnWebApiAuth.API.Startup))]
 
@@ -12,9 +16,26 @@ namespace LearnWebApiAuth.API
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
+            app.UseCors(CorsOptions.AllowAll);
+
             HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
+            WebApiConfig.Register(config);            
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
