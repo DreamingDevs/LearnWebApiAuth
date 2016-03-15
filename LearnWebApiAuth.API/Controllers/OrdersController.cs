eff2cb847b8f0ab958f2f56dfdb4dc51d0635406
RamiV <rami.ramilu@gmail.com>
2016-03-15 11:29:01 +0530

    Added - 1. Account Controller 2. Order Controller 3. Order Model Class

---
+++ LearnWebApiAuth.API/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using LearnWebApiAuth.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearnWebApiAuth.API.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            List<Order> OrderList = new List<Order>
+            {
+                new Order {OrderID = 10248, CustomerName = "Rami Vemula", ShipperCity = "Hyderabad", IsShipped = true },
+                new Order {OrderID = 10249, CustomerName = "Ramilu", ShipperCity = "Visakhapatnam", IsShipped = false}
+            };
+            return Ok(OrderList);
+        }
+
+    }
+}
