d416cf1cc214b0914f08dc8543e54c975a162a15
RamiV <rami.ramilu@gmail.com>
2016-03-23 20:42:52 +0530

    Added - 1. Refresh Token Entities 2. DBSet for RefreshToken Entities 3. Repo methods for Refresh Tokens

--- LearnWebApiAuth.API/AuthContext.cs
+++ LearnWebApiAuth.API/AuthContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using LearnWebApiAuth.API.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +15,8 @@ namespace LearnWebApiAuth.API
         {
 
         }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
